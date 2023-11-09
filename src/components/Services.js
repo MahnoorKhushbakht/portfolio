7fcdccbc2143f8a0a8b1151a3ccd933c4422ba5f
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2023-11-10 01:16:01 +0500

    updated

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,6 +5,7 @@ import Card2 from './Cards/Card2';
 import Card3 from './Cards/Card3';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Typewriter from 'typewriter-effect';
 export default function Variants() {
   const containerStyles = {
     display: 'flex',
@@ -16,7 +17,14 @@ export default function Variants() {
   return (
     <Container id="services" sx={containerStyles}>
       <Typography variant="h4" style={{ marginBottom: '20px', color: '#F6F1EE' ,fontFamily: "'Courier New', Courier, monospace"}}>
-        Career Snapshot
+      <Typewriter 
+ onInit={(typewriter) => {
+     typewriter
+         .typeString("Career Snapshot")
+         .start();
+ }}
+
+/>
       </Typography>
       <Stack
         direction={{ xs: 'column', sm: 'row' }}
