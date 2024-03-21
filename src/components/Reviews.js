f710e24e537cbd912e99784477c5f7b169df595c
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-03-21 16:04:03 +0500

    updated

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -16,7 +16,7 @@ export default function Reviews(){
 
         <Box>
           <Heading size='sm' textAlign='left' fontFamily="'Courier New', Courier, monospace">Promise Emmanuel</Heading>
-          <Text>Digital Marketer, Nigeria</Text>
+          <Text fontSize={{ base: "14px", md: "14px", lg: "16px" }}>Digital Marketer, Nigeria</Text>
         </Box>
       </Flex>
       <IconButton
@@ -28,7 +28,7 @@ export default function Reviews(){
     </Flex>
   </CardHeader>
   <CardBody>
-    <Text display='flex' alignContent='left' textAlign='left'>
+    <Text display='flex' alignContent='left' textAlign='left' fontSize={{ base: "14px", md: "14px", lg: "16px" }}>
     I'd like to give a special shout-out to Mahnoor khushbakht, who created a professional and eye-catching portfolio website. Her expertise and attention to detail were invaluable in creating a site that truly reflects my work and style. I'm so grateful for her help and I highly recommend her to anyone looking for website developer services.
     </Text>
   </CardBody>
