bd02c56b37caee616fec1195466874812a86865d
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-03-21 16:49:55 +0500

    updated

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -8,11 +8,11 @@ export default function Reviews(){
     <ChakraProvider>
   <div id="review" height="100vh" >
   <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" marginTop='100px'  marginBottom='20px'>
-    <Card backgroundColor='#F6F1EE' sx={{width: { base: '85%', sm: '80%', md: '60%', lg: '50%' }}}>
+    <Card backgroundColor='#F6F1EE' sx={{width: { base: '85%', sm: '85%', md: '60%', lg: '50%' }}}>
   <CardHeader>
     <Flex spacing='2'>
       <Flex flex='1' gap='2' alignItems='left' flexWrap='wrap'>
-        <Avatar fontSize={{ base: "2px", md: "6px", lg: "6px" }} name='Promise Emmanuel' src={ClientImage} />
+        <Avatar  name='Promise Emmanuel' src={ClientImage} />
 
         <Box>
           <Heading size='sm' textAlign='left' fontFamily="'Courier New', Courier, monospace">Promise Emmanuel</Heading>
@@ -28,7 +28,7 @@ export default function Reviews(){
     </Flex>
   </CardHeader>
   <CardBody>
-    <Text display='flex' alignContent='left' textAlign='left' fontSize={{ base: "12px", md: "12px", lg: "16px" }}>
+    <Text display='flex' alignContent='left' textAlign='left' fontSize={{ base: "14px", md: "14px", lg: "16px" }}>
     I'd like to give a special shout-out to Mahnoor khushbakht, who created a professional and eye-catching portfolio website. Her expertise and attention to detail were invaluable in creating a site that truly reflects my work and style. I'm so grateful for her help and I highly recommend her to anyone looking for website developer services.
     </Text>
   </CardBody>
