8c6592442f29a7e59f9f3a0611b975a4a17a1e4b
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-03-21 16:24:24 +0500

    updated

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,6 +1,5 @@
-import { Card, CardHeader, CardBody, CardFooter,Flex,Avatar,IconButton,Box,Heading,Text,Button } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter,Flex,Avatar,Box,Heading,Text,Button } from '@chakra-ui/react'
 import { BiLike, BiChat,BiShare } from 'react-icons/bi'
-import { BsThreeDotsVertical } from 'react-icons/bs';
 import ClientImage from '../images/ClientImage.jpeg';
 import { ChakraProvider } from '@chakra-ui/react';
 export default function Reviews(){
@@ -16,19 +15,13 @@ export default function Reviews(){
 
         <Box>
           <Heading size='sm' textAlign='left' fontFamily="'Courier New', Courier, monospace">Promise Emmanuel</Heading>
-          <Text fontSize={{ base: "12px", md: "12px", lg: "16px" }}>Digital Marketer, Nigeria</Text>
+          <Text fontSize={{ base: "14px", md: "14px", lg: "16px" }}>Digital Marketer, Nigeria</Text>
         </Box>
       </Flex>
-      <IconButton
-        variant='ghost'
-        colorScheme='gray'
-        aria-label='See menu'
-        icon={<BsThreeDotsVertical />}
-      />
     </Flex>
   </CardHeader>
   <CardBody>
-    <Text display='flex' alignContent='left' textAlign='left' fontSize={{ base: "12px", md: "12px", lg: "16px" }}>
+    <Text display='flex' alignContent='left' textAlign='left' fontSize={{ base: "14px", md: "14px", lg: "16px" }}>
     I'd like to give a special shout-out to Mahnoor khushbakht, who created a professional and eye-catching portfolio website. Her expertise and attention to detail were invaluable in creating a site that truly reflects my work and style. I'm so grateful for her help and I highly recommend her to anyone looking for website developer services.
     </Text>
   </CardBody>
