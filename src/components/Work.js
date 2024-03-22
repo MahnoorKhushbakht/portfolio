a2beaacf44f86888592bc1648b58fc5e5e2be38c
mahnoor <mahnoor.bscs3429@iiu.edu.pl>
2024-03-22 19:57:45 +0500

    updated

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,17 +5,17 @@ import wb from '../images/wb.JPG';
 
 export default function Work() {
     const data = [
-        { src: gd, alt: 'Graphic Design',heading:'Graphic Design' ,url: 'https://github.com/MahnoorKhushbakht/GraphicDesign/tree/master' ,text:' Researched, Assisted and designed innovative ideas for brand development. Coordinated with clients to craft creative advertising content suitable for the target market.'},
-        { src: wb, alt: 'Website Development',heading:'Website Development', url: 'https://github.com/MahnoorKhushbakht/pe_portfolio_website',text:'In my role as a web developer, I have accumulated valuable experience in crafting responsive and user-friendly web applications.' },
-        { src: amz, alt: 'Virtual Assistant',heading:'Virtual Assistant', url: 'https://github.com/MahnoorKhushbakht/Virtual-Assistant-Portfolio',text:'Researched 20+ promising products as per KPIs set by the client. Calculated profit margin of the product after getting Quotation for the Supplier.' }
+        { src: gd, alt: 'Graphic Design', heading: 'Graphic Design', url: 'https://github.com/MahnoorKhushbakht/GraphicDesign/tree/master', text: 'Researched, Assisted and designed innovative ideas for brand development. Coordinated with clients to craft creative advertising content suitable for the target market.' },
+        { src: wb, alt: 'Website Development', heading: 'Website Development', url: 'https://github.com/MahnoorKhushbakht/pe_portfolio_website', text: 'In my role as a web developer, I have accumulated valuable experience in crafting responsive and user-friendly web applications.' },
+        { src: amz, alt: 'Virtual Assistant', heading: 'Virtual Assistant', url: 'https://github.com/MahnoorKhushbakht/Virtual-Assistant-Portfolio', text: 'Researched 20+ promising products as per KPIs set by the client. Calculated profit margin of the product after getting Quotation for the Supplier.' }
     ];
 
     return (
         <div id='work' height='100vh'>
             <ChakraProvider>
-                <Box display="flex" justifyContent="center" flexDirection={{ base:'column',sm: 'column', md: 'row' }} alignItems="center" minHeight="100vh" marginTop='130px' marginBottom='20px' spacing="4">
+                <Box display="flex" justifyContent="center" flexDirection={{ base: 'column', sm: 'column', md: 'row' }} alignItems="center" minHeight="100vh" marginTop='130px' marginBottom='20px' spacing="4">
                     {data.map((item, index) => (
-                        <Box key={index} mx={{ base: 'auto', md: '4' }} mb={{ base: '4', md: '0' }}>
+                        <Box key={index} mx={{ base: 'auto', md: '4' }} mb={{ base: '4', md: '0' }} ml={{ base: '8', md: '0' }} mr={{ base: '8', md: '0' }}>
                             <Card maxW='sm' backgroundColor="#4F4A45" >
                                 <CardBody>
                                     <Image
@@ -26,11 +26,11 @@ export default function Work() {
                                         width={index === 0 ? '350px' : 'auto'} // adjust width for the gd image
                                     />
                                     <Stack mt='6' spacing='3'>
-                                        <Heading size='md' style={{ color: '#F6F1EE', fontFamily:"'Courier New', Courier, monospace"}}>{item.heading}</Heading>
-                                        <Text style={{ color: '#F6F1EE'}}>
-                                        {item.text}
+                                        <Heading size='md' style={{ color: '#F6F1EE', fontFamily: "'Courier New', Courier, monospace" }}>{item.heading}</Heading>
+                                        <Text style={{ color: '#F6F1EE' }}>
+                                            {item.text}
                                         </Text>
-                                        <Button as='a' style={{ color: '#F6F1EE',backgroundColor: "#ED7D31"}}href={item.url} variant='solid' colorScheme='blue' target='_blank'>
+                                        <Button as='a' style={{ color: '#F6F1EE', backgroundColor: "#ED7D31" }} href={item.url} variant='solid' colorScheme='blue' target='_blank'>
                                             Learn More
                                         </Button>
                                     </Stack>
